369a01b1df5b6aa790a95ffca4350c2466b64dfd
Kira <kira.green@btinternet.com>
2019-04-08 20:31:03 +0100

    added methods to post delete and patch

--- routes/players.js
+++ routes/players.js
@@ -15,6 +15,57 @@ router.get("/", async (req, res, next) => {
 		});
 	}
 });
-/* GET users listing. */
+
+router.get("/:id", async (req, res, next) => {
+	try {
+		const player = await Player.findOne({ID : req.params.id});
+		res.json({ payload: { player } });
+	} catch (err) {
+		res.status(500).json({
+			message: "Error finding player in database",
+			error: err
+		});
+	}
+});
+
+router.post("/", async (req, res, next) => {
+	try {
+		const player = new Player(req.body)
+		await player.save();
+		res.json({ payload: { player} });
+	} catch (err) {
+		res.status(500).json({
+			message: "Error saving player to database",
+			error: err
+		});
+	}
+});
+
+router.delete("/:id", async (req, res, next) => {
+	try {
+		const player = await Player.deleteOne({ID : req.params.id});
+		res.json({ message: "player deleted" });
+		
+	} catch (err) {
+		res.status(500).json({
+			message: "Error deleting player from database",
+			error: err
+		});
+	}
+});
+
+router.patch("/:id", async (req, res, next) => {
+	try {
+		const player = await Player.findOneAndUpdate({ID : req.params.id});
+		res.json({ payload: { player } });
+	} catch (err) {
+		res.status(500).json({
+			message: "Error updating player in database",
+			error: err
+		});
+	}
+});
+
+
 
 module.exports = router;
